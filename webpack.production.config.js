732087881e075213a2428e2655bbd2169e9d8522
Trond Klakken <trond@klakken.com>
2015-11-22 13:26:25 +0100

    Trenger mer jobbing rundt deploy. Se kommentar
    Må ryddes opp i hva som er produksjon og hva som er develop. Nå er alt
    ett sabla rot

---
+++ webpack.production.config.js
@@ -0,0 +1,48 @@
+var webpack = require('webpack');
+var path = require('path');
+var node_modules_dir = path.resolve(__dirname, 'node_modules');
+
+var config = {
+  entry: [
+    './js/index'
+  ],
+  output: {
+    path: __dirname + '/build/',
+    filename: '[name].entry.js'
+  },
+  plugins: [
+    new webpack.NoErrorsPlugin()
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: [node_modules_dir],
+        loaders: [
+          'react-hot',
+          'babel-loader'
+        ]
+      },
+      {
+        test: /\.less$/,
+        exclude: /dev\.less$/,
+        loaders: [
+          "style-loader",
+          "css-loader",
+          "autoprefixer-loader?browsers=last 2 version",
+          "less-loader?strictMath&cleancss"
+        ]
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader?limit=10000&minetype=application/font-woff"
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "file-loader"
+      }
+    ]
+  }
+};
+
+module.exports = config;
